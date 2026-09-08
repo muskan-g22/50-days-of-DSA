3000d0e02b0d5c88dc4e42a77c57364b897e9317
muskan <muskan22110@gmail.com>
2026-09-08 19:16:11 +0530

    Bubble sort

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,4 +1,4 @@
-def bubble_sort(arr):
+def bubblesort(arr):
     n = len(arr)
 
     for i in range(n):
@@ -11,5 +11,29 @@ def bubble_sort(arr):
 
 
 arr = [5, 3, 8, 4, 2]
+
+print(bubblesort(arr))
+
+# optimized
+
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        swapped = False
+
+        for j in range(0, n - i - 1):
+
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+    return arr
+
+
+arr = [1, 2, 3, 4, 5]
 
 print(bubble_sort(arr))
