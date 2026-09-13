ffd76844350d8c3ac0205d08642c5fe21dd86012
muskan <muskan22110@gmail.com>
2026-09-13 19:53:17 +0530

    20 - Linked List

--- d20.py
+++ d20.py
@@ -1,50 +1,216 @@
-# Problem
 
-# You are given an n × n matrix where:
-
-# Every row is sorted in ascending order.
-# Every column is sorted in ascending order.
-
-# Find the kth smallest element.
+# LINKED LIST PROBLEMS 
 
 
-import heapq
-
-def kthSmallest(matrix, k):
-    heap = []
-
-    n = len(matrix)
-
-    # Add first element of every row
-    for row in range(n):
-        heapq.heappush(heap, (matrix[row][0], row, 0))
-
-    # Remove smallest k times
-    for _ in range(k):
-        value, row, col = heapq.heappop(heap)
-
-        # Add next element from the same row
-        if col + 1 < n:
-            heapq.heappush(
-                heap,
-                (matrix[row][col + 1], row, col + 1)
-            )
-
-    return value
+# Node of Linked List
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
-n = int(input("Enter matrix size: "))
+# Create Linked List 
+def createList(arr):
+    if not arr:
+        return None
 
-matrix = []
+    head = ListNode(arr[0])
+    curr = head
 
-print("Enter matrix:")
+    for value in arr[1:]:
+        curr.next = ListNode(value)
+        curr = curr.next
 
-for i in range(n):
-    row = list(map(int, input().split()))
-    matrix.append(row)
+    return head
 
-k = int(input("Enter k: "))
 
-answer = kthSmallest(matrix, k)
+# Print Linked List
+def printList(head):
+    curr = head
 
-print("Kth smallest element:", answer)
+    while curr:
+        print(curr.val, end=" -> ")
+        curr = curr.next
+
+    print("None")
+
+
+# Reverse Linked List
+
+
+def reverseList(head):
+    prev = None
+    curr = head
+
+    while curr:
+        next_node = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next_node
+
+    return prev
+
+
+#  17. Middle of Linked List
+
+def middleNode(head):
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    return slow
+
+
+#  Linked List Cycle
+
+
+def hasCycle(head):
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            return True
+
+    return False
+
+
+#  Merge Two Sorted Lists
+
+
+def mergeTwoLists(list1, list2):
+    dummy = ListNode(0)
+    curr = dummy
+
+    while list1 and list2:
+
+        if list1.val <= list2.val:
+            curr.next = list1
+            list1 = list1.next
+
+        else:
+            curr.next = list2
+            list2 = list2.next
+
+        curr = curr.next
+
+    # Attach remaining nodes
+    if list1:
+        curr.next = list1
+    else:
+        curr.next = list2
+
+    return dummy.next
+
+
+# Remove Nth Node From End
+
+
+def removeNthFromEnd(head, n):
+
+    dummy = ListNode(0, head)
+
+    slow = dummy
+    fast = dummy
+
+    # Move fast n steps ahead
+    for _ in range(n):
+        fast = fast.next
+
+    # Move both pointers
+    while fast.next:
+        slow = slow.next
+        fast = fast.next
+
+    # Remove node
+    slow.next = slow.next.next
+
+    return dummy.next
+
+
+
+
+if __name__ == "__main__":
+
+   
+    print("\n Reverse Linked List")
+
+    head = createList([1, 2, 3, 4, 5])
+
+    print("Original:")
+    printList(head)
+
+    head = reverseList(head)
+
+    print("Reversed:")
+    printList(head)
+
+
+    print("\n Middle of Linked List")
+
+    head = createList([1, 2, 3, 4, 5])
+
+    print("Linked List:")
+    printList(head)
+
+    middle = middleNode(head)
+
+    print("Middle:", middle.val)
+
+
+    
+
+    print("\n Linked List Cycle")
+
+    head = createList([1, 2, 3, 4, 5])
+
+    # Create a cycle:
+    # 5 -> 3
+    node3 = head.next.next
+    node5 = head.next.next.next.next
+
+    node5.next = node3
+
+    print("Cycle exists:", hasCycle(head))
+
+
+   
+
+    print("\n Merge Two Sorted Lists")
+
+    list1 = createList([1, 3, 5])
+    list2 = createList([2, 4, 6])
+
+    print("List 1:")
+    printList(list1)
+
+    print("List 2:")
+    printList(list2)
+
+    merged = mergeTwoLists(list1, list2)
+
+    print("Merged:")
+    printList(merged)
+
+
+    
+
+    print("\n Remove Nth Node From End")
+
+    head = createList([1, 2, 3, 4, 5])
+
+    print("Original:")
+    printList(head)
+
+    n = 2
+
+    head = removeNthFromEnd(head, n)
+
+    print("After removing", n, "node from end:")
+    printList(head)
